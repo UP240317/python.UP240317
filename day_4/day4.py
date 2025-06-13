debfc178af39d8ab91eaad2319c6acd3c12db304
UP240317 <up240317@alumnos.upa.edu.mx>
2025-06-13 13:07:57 -0600

    dia_4_python

--- day_4/day4.py
+++ day_4/day4.py
@@ -48,3 +48,98 @@ print(word)
 words="Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
 list_companies = words.split(' ')
 print(list_companies)
+#ejercicio 15
+print(company[0])
+#ejercicio 16
+print(company[13])
+#ejercicio 17
+print(company[10])
+#ejercicio 18
+phrase1='Python'
+phrase2='For'
+phrase3='All'
+X=(phrase1[0])
+Y=(phrase2[0])
+Z=(phrase3[0])
+all_text=print(X+Y+Z)
+#ejercicio 19
+ph1='Coding'
+ph2='For'
+ph3='All'
+a=(ph1[0])
+b=(ph2[0])
+c=(ph3[0])
+all_text=print(a+b+c)
+#ejercicio 20
+print(company.index('c'))
+#ejercicio 21
+print(company.index('f'))
+#ejercicio 22
+print(company.index('l'))
+#ejercicio 23
+sentence='You cannot end a sentence with because because because is a conjunction'
+print(sentence.index('because'))
+#ejercicio 24
+print(sentence.rindex('because'))
+#ejercicio 25
+phrase_to_remove = 'because because because'
+new_sentence = sentence.replace(phrase_to_remove, '')
+print(new_sentence)
+#ejercicio 26
+position = sentence.find('because')
+print(position)
+#ejercicio 27
+start = sentence.find('because because because')
+end = start + len('because because because')
+result = sentence[start:end]
+print(result)
+#ejercicio 28
+textos='coding for all'
+starts_with_coding = textos.startswith('coding')
+print(starts_with_coding)
+#ejercicio 29
+ends_with_coding = textos.endswith('coding')
+print(ends_with_coding)
+#ejercicio 30
+espacios=" Coding for all "
+print(espacios.strip())
+#ejercicio 31
+var1 = "30DaysOfPython"
+var2 = "thirty_days_of_python"
+
+print(var1, "is", var1.isidentifier())  
+print(var2, "is", var2.isidentifier())  
+#Actividad 32
+librerias = ["Django", "Flask", "Bottle", "Pyramid", "Falcon"]
+unidos= "las siguientes son librarias:%s" % (librerias)
+print(unidos) 
+
+
+#Actividad 33
+print("I am enjoying this challenge. \n I just wonder what is next.")
+
+
+#Actividad 34
+print("\tname     \tage    \tcountry")
+print("\tcity")
+print("\tAsabeneh \t250 \tFinland")
+print("\tHelsinki")
+
+
+#ACtividad 35
+radius = 10
+area = 3.14 * radius ** 2
+print("el area del circulo con un radio de: %d es: %.2f" %(radius,area))
+
+
+#Actividad 36
+a=8
+b=6
+print("{} + {} = {}".format(a,b,a+b) )
+print("{} - {} = {}".format(a,b,a-b) )
+print("{} x {} = {}".format(a,b,a*b) )
+print("{} / {} = {:.2f}".format(a,b,a/b) )
+print("{} % {} = {}".format(a,b,a%b) )
+print("{} // {} = {}".format(a,b,a//b))
+print("{} ** {} = {}".format(a,b,a**b))
+#final
